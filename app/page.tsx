e1b21c9ee4d3dec903b7e0e95a9e0e23bc0d45c9
myk <michaelmugendi1707@gmail.com>
2024-02-02 19:45:42 +0300

    Add: added convex db and clerk plus middle ware

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Hello Board</p>;
+  return <Button>Click me!</Button>;
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://workable-bison-16.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
