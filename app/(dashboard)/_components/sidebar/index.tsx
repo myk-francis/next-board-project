02af99a1198c81ee1343cf726a60445abf62e3b0
myk <michaelmugendi1707@gmail.com>
2024-02-03 10:58:24 +0300

    Add: add tooltip and dialog plus new button component

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NewButton from "./new-button";
 
 export const Sidebar = () => {
   return (
     <div className="fixed left-0 z-[1] bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4 text-white">
-      Side
+      <NewButton />
     </div>
   );
 };
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewButton = () => {
+  return <div>NewButton</div>;
+};
+
+export default NewButton;
