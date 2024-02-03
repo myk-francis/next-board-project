e46562eaa6b9b00688ed50d7a3bf55a3706121f7
myk <michaelmugendi1707@gmail.com>
2024-02-03 11:42:40 +0300

    Update: item component for organisation finished

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
 import { NewButton } from "./new-button";
+import { List } from "./list";
 
 export const Sidebar = () => {
   return (
     <div className="fixed left-0 z-[1] bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4 text-white">
+      <List />
       <NewButton />
     </div>
   );
