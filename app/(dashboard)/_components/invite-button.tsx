cfd5c3b71d600652cf55421971fd32bdaba82379
myk <michaelmugendi1707@gmail.com>
2024-02-04 11:40:33 +0300

    Update: finished invite button

--- app/(dashboard)/_components/invite-button.tsx
+++ app/(dashboard)/_components/invite-button.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Plus } from "lucide-react";
+import { OrganizationProfile } from "@clerk/nextjs";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 
 function InviteButton() {
-  return <div>InviteButton</div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"outline"} className="h-4 w-4 mr-2">
+          <Plus className="text-white" />
+          Invite members
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+        <OrganizationProfile />
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default InviteButton;
