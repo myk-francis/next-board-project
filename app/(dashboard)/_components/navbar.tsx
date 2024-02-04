7e3536117c261ca4fcdad124ae0bc26c79b73135
myk <michaelmugendi1707@gmail.com>
2024-02-04 10:26:44 +0300

    Update: added shadcn inputs

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,13 +2,14 @@
 
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import { SearchInput } from "./search-input";
 
 export const NavBar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         {/* TODO: Add search */}
-        Search
+        <SearchInput />
       </div>
       <UserButton />
     </div>
--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,1 +1,11 @@
 "use client";
+
+import React from "react";
+import qs from "query-string";
+import { Search } from "lucide-react";
+import { useDebounceCallback } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+
+export const SearchInput = () => {
+  return <div>SearchInput</div>;
+};
