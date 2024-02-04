ac2072744acdca58efe9c4a1986b93d201800d23
myk <michaelmugendi1707@gmail.com>
2024-02-04 13:54:44 +0300

    Update: added board card component

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { EmptySearch } from "./empty-search";
 import { EmptyFavorites } from "./empty-favorites";
 import { EmptyBoards } from "./empty-boards";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 interface BoardListProps {
   orgId: string;
@@ -14,7 +16,11 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data: any = []; //TODO: Change to API call
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined) {
+    return <div>Loading...</div>;
+  }
 
   if (!data?.length && query?.search) {
     return <EmptySearch />;
@@ -28,5 +34,12 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
     return <EmptyBoards />;
   }
 
-  return <div>{JSON.stringify(query)}</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">
+        {query.favorites ? "Favorite boards" : "Team boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10"></div>
+    </div>
+  );
 };
