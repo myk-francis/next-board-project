61762cf9323fba41a1a7dacfbc8e96f602752ca2
myk <michaelmugendi1707@gmail.com>
2024-02-04 13:23:46 +0300

    Update: finished apimutation hook

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -5,15 +5,16 @@ import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { useMutation } from "convex/react";
 import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
   const { organization } = useOrganization();
-  const create = useMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
 
   const onClick = () => {
     if (!organization) return;
 
-    create({
+    mutate({
       orgId: organization?.id,
       title: "Untitled",
     });
@@ -27,7 +28,7 @@ export const EmptyBoards = () => {
         Start by creating a board for your organisation
       </p>
       <div className="mt-6">
-        <Button onClick={onClick} size={"lg"}>
+        <Button disabled={pending} onClick={onClick} size={"lg"}>
           Create Board
         </Button>
       </div>
