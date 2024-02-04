e9ffd52f0fb65e36e94fb04c4ba545542a3a1bbd
myk <michaelmugendi1707@gmail.com>
2024-02-04 12:29:49 +0300

    Update: added assets

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import React from "react";
+
 interface BoardListProps {
   orgId: string;
   query: {
@@ -8,8 +10,20 @@ interface BoardListProps {
   };
 }
 
-import React from "react";
-
 export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data: any = []; //TODO: Change to API call
+
+  if (!data?.length && query?.search) {
+    return <div>Try searching for something else</div>;
+  }
+
+  if (!data?.length && query?.favorites) {
+    return <div>No favorites</div>;
+  }
+
+  if (!data?.length) {
+    return <div>No boards found</div>;
+  }
+
   return <div>{JSON.stringify(query)}</div>;
 };
