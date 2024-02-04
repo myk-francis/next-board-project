0126c87d9023ca0d5bfcb658040beaa25ba29e4e
myk <michaelmugendi1707@gmail.com>
2024-02-04 09:48:24 +0300

    Update: installed query string and usehooks

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export const NavBar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
         {/* TODO: Add search */}
         Search
