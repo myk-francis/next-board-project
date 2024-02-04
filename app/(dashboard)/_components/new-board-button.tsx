ac5a8a87afb5c5a989d11f9365a114245ff75e3c
myk <michaelmugendi1707@gmail.com>
2024-02-04 16:05:42 +0300

    Update: finished implementation of new board

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { cn } from "@/lib/utils";
+import { Plus } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 interface NewBoardButtonProps {
   orgId: string;
@@ -8,5 +13,31 @@ interface NewBoardButtonProps {
 }
 
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
-  return <div>NewBoardButton</div>;
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    mutate({ orgId, title: "Untitled" })
+      .then((id) => {
+        toast.success("Board created");
+        //TODO: Redirect ot board/{id}
+      })
+      .catch((error) => {
+        toast.error("Failed to create board!");
+      });
+  };
+
+  return (
+    <button
+      disabled={pending || disabled}
+      onClick={onClick}
+      className={cn(
+        "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
+        (pending || disabled) && "cursor-not-allowed opacity-75"
+      )}
+    >
+      <div />
+      <Plus className="h-12 w-12 text-white stroke-1" />
+      <p className="text-sm text-white font-light">New Board</p>
+    </button>
+  );
 };
