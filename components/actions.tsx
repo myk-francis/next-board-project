39ab437e7e74667c6ae2fb9e92b75d9ef23f2774
myk <michaelmugendi1707@gmail.com>
2024-02-04 16:41:33 +0300

    Update: added one menu action

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,6 +1,14 @@
 "use client";
-
+import React from "react";
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Link2 } from "lucide-react";
+import { toast } from "sonner";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -9,3 +17,39 @@ interface ActionsProps {
   id: string;
   title: string;
 }
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => {
+        toast.success("Link copied");
+      })
+      .catch((error) => {
+        toast.error("Failed to copy link");
+      });
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        className="w-60"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="cursor-pointer p-3 ">
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
