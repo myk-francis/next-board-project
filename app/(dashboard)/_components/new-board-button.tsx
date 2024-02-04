84c3288bb66c0bd24d52b9afb6415677ead72f1d
myk <michaelmugendi1707@gmail.com>
2024-02-04 15:53:17 +0300

    Update: added new board button component

---
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+interface NewBoardButtonProps {
+  orgId: string;
+  disabled?: boolean;
+}
+
+export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
+  return <div>NewBoardButton</div>;
+};
