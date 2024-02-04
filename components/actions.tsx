d0cd0e1bee0b168ae9b4c596035f279901e1cd78
myk <michaelmugendi1707@gmail.com>
2024-02-04 16:22:25 +0300

    Update: added actions file

---
+++ components/actions.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
