56cf279a2bef2c4d74d6c3c755a83cf2b9546d19
myk <michaelmugendi1707@gmail.com>
2024-02-04 14:03:46 +0300

    Update: finished board card

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,7 +1,37 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-export const BoardCard = () => {
-  return <div>BoardCard</div>;
+interface BoardCardProps {
+  id: string;
+  title: string;
+  authorName: string;
+  authorId: string;
+  createdAt: number;
+  imageUrl: string;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({
+  id,
+  title,
+  authorName,
+  authorId,
+  createdAt,
+  imageUrl,
+  orgId,
+  isFavorite,
+}: BoardCardProps) => {
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt="Doodle" className="object-fit" fill />
+        </div>
+      </div>
+    </Link>
+  );
 };
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyFavorites } from "./empty-favorites";
 import { EmptyBoards } from "./empty-boards";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { BoardCard } from "./board-card";
 
 interface BoardListProps {
   orgId: string;
@@ -39,7 +40,21 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
       <h2 className="text-3xl">
         {query.favorites ? "Favorite boards" : "Team boards"}
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        {data.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
     </div>
   );
 };
