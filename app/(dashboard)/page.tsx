a4561281567d9a2f1c1c94624bc8b31ab58b946a
myk <michaelmugendi1707@gmail.com>
2024-02-04 12:10:13 +0300

    Update: finished empty state

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 
 export default function DashboardPage() {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex-1 h-[calc(100%-80px)]">
-      <EmptyOrg />
+      {!organization ? <EmptyOrg /> : <p>Board List!</p>}
     </div>
   );
 }
