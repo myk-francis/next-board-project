ba56edad95232502201623084c2d998284ce1b58
myk <michaelmugendi1707@gmail.com>
2024-02-05 16:14:44 +0300

    Update: added boards with favs list

--- convex/boards.ts
+++ convex/boards.ts
@@ -18,6 +18,23 @@ export const get = query({
       .order("desc")
       .collect();
 
-    return boards;
+    const boardsWithFavoritesRelation = boards.map((board) => {
+      return ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) =>
+          q.eq("userId", identity.subject).eq("boardId", board._id)
+        )
+        .unique()
+        .then((favorite) => {
+          return {
+            ...board,
+            isFavorite: !!favorite,
+          };
+        });
+    });
+
+    const boardsWithFavorites = await Promise.all(boardsWithFavoritesRelation);
+
+    return boardsWithFavorites;
   },
 });
