69619958a90fdb02ee6098b8bbde37a330b232e9
myk <michaelmugendi1707@gmail.com>
2024-02-07 17:44:57 +0300

    Update: added variant to button

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,17 +4,15 @@ import React from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { ToolBar } from "./toolbar";
-import { useSelf } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-  const info = useSelf((me) => me.info);
   return (
     <main className="h-full w-full bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <ToolBar />
     </main>
--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,10 +1,33 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
 import React from "react";
 
-export const Info = () => {
+interface InfoProps {
+  boardId: string;
+}
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export const Info = ({ boardId }: InfoProps) => {
+  const data = useQuery(api.board.getOne, { id: boardId as Id<"boards"> });
+
+  if (!data) {
+    return <InfoSkeleton />;
+  }
+
   return (
     <div className="h-12 absolute top-2 left-2 bg-white rounded-md px-1.5 flex items-center shadow-md">
-      TODO: Information about board
+      <Button className="px-2">
+        <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
+      </Button>
     </div>
   );
 };
