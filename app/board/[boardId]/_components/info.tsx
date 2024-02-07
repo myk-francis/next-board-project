1d131ee5e029386f2669a70b43b3f939ab25db7e
myk <michaelmugendi1707@gmail.com>
2024-02-07 10:06:15 +0300

    Update: added styling to component

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Info = () => {
-  return <div>Info</div>;
+  return (
+    <div className="h-12 absolute top-2 left-2 bg-white rounded-md px-1.5 flex items-center shadow-md">
+      Info
+    </div>
+  );
 };
