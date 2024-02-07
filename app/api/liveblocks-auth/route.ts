d04b7d53ca6730a85630058e58ae450f216541a9
myk <michaelmugendi1707@gmail.com>
2024-02-07 17:18:05 +0300

    Update: added statuscode

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -35,7 +35,7 @@ export async function POST(request: Request) {
   });
 
   if (board?.orgId !== authorization.orgId) {
-    return new Response("Unauthorized");
+    return new Response("Unauthorized", { status: 403 });
   }
 
   const userInfo = {
