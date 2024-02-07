c34919719d25a56ef8d360d495310a3fd93a9c54
myk <michaelmugendi1707@gmail.com>
2024-02-07 10:21:27 +0300

    Update: added added divs to toolbar component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,12 +3,14 @@
 import React from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
+import { ToolBar } from "./toolbar";
 
 export const Canvas = () => {
   return (
     <main className="h-full w-full bg-neutral-100 touch-none">
       <Info />
       <Participants />
+      <ToolBar />
     </main>
   );
 };
