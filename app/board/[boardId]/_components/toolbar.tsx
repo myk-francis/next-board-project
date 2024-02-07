cec4cb1446b7a7ae0f74ec0a3c34582da69df722
myk <michaelmugendi1707@gmail.com>
2024-02-07 11:12:46 +0300

    Update: added toolbar component

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 export const ToolBar = () => {
@@ -13,6 +14,14 @@ export const ToolBar = () => {
         <div>Undo</div>
         <div>Redo</div>
       </div>
+    </div>
+  );
+};
+
+ToolBar.Skeleton = function ToolBarSkeleton() {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px]">
+      <Skeleton className="h-full w-full bg-muted-400" />
     </div>
   );
 };
