9b479b17ebcc717c38c18e55f7bd213eacd271c8
myk <michaelmugendi1707@gmail.com>
2024-02-07 10:11:52 +0300

    Update: added toolbar component

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Participants = () => {
   return (
     <div className="h-12 absolute top-2 right-2 bg-white rounded-md px-3 flex items-center shadow-md">
-      Participants
+      List of users
     </div>
   );
 };
