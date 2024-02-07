e5c08381afc85b198b697ba0fe59fc606440986e
myk <michaelmugendi1707@gmail.com>
2024-02-07 10:03:49 +0300

    Update: added Info component

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
+import { Info } from "./info";
 
 export const Canvas = () => {
-  return <div className="h-full w-full bg-neutral-100 touch-none">Canvas</div>;
+  return (
+    <main className="h-full w-full bg-neutral-100 touch-none">
+      <Info />
+    </main>
+  );
 };
