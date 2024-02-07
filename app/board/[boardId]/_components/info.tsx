fa07c87b60b177bfd7aa19717502f6122d5c06a3
myk <michaelmugendi1707@gmail.com>
2024-02-07 17:53:11 +0300

    Update: added hint and link on button info

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
 import { useQuery } from "convex/react";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface InfoProps {
@@ -25,9 +28,21 @@ export const Info = ({ boardId }: InfoProps) => {
 
   return (
     <div className="h-12 absolute top-2 left-2 bg-white rounded-md px-1.5 flex items-center shadow-md">
-      <Button className="px-2">
-        <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
-      </Button>
+      <Hint label="Home" side="bottom" sideOffset={10}>
+        <Button asChild variant={"board"} className="px-2">
+          <Link href={`/`}>
+            <Image src="/logo.svg" alt="Board Logo" height={40} width={40} />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
     </div>
   );
 };
