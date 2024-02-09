55525a728b72d9792daf208ff001cb9e4f42ce4a
myk <michaelmugendi1707@gmail.com>
2024-02-09 09:31:35 +0300

    Update: implemented different border colors

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -2,6 +2,7 @@
 import { useOthers, useSelf } from "@/liveblocks.config";
 import React from "react";
 import { UserAvatar } from "./user-avatar";
+import { connectionIdToColor } from "@/lib/utils";
 
 const MAX_SHOWN_USERS = 2;
 
@@ -16,6 +17,7 @@ export const Participants = () => {
         {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
           return (
             <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
               key={connectionId}
               src={info?.picture}
               name={info?.name}
@@ -26,6 +28,7 @@ export const Participants = () => {
 
         {currentUser && (
           <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
             src={currentUser?.info?.picture}
             name={`${currentUser?.info?.name} (You)`}
             fallback={currentUser?.info?.name?.[0]}
