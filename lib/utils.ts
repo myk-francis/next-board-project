8cdd9d9c49bd14b53a85c6ddf9497c0dc4f66985
myk <michaelmugendi1707@gmail.com>
2024-02-09 09:29:32 +0300

    Update: added connectionid function

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,3 +6,7 @@ const COLORS = ["#DC2626", "#9333EA", "#2563EB", "#059669", "#7E22CE"];
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+}
