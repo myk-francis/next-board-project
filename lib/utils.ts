d00305d9b5c006080871f125ceb6ec0b4f39fb7e
myk <michaelmugendi1707@gmail.com>
2024-02-09 09:27:35 +0300

    Update: added array of colors

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = ["#DC2626", "#9333EA", "#2563EB", "#059669", "#7E22CE"];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
