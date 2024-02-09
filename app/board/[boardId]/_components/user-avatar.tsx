3bce2f78472e401383fbe7b2c3b38e230c4aafb5
myk <michaelmugendi1707@gmail.com>
2024-02-09 09:09:52 +0300

    Update: finished user-avatar

--- app/board/[boardId]/_components/user-avatar.tsx
+++ app/board/[boardId]/_components/user-avatar.tsx
@@ -1,5 +1,28 @@
+import { Hint } from "@/components/hint";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
 
-export const UserAvatar = () => {
-  return <div>UserAvatar</div>;
+interface UserAvatarProps {
+  src?: string;
+  name?: string;
+  fallback?: string;
+  borderColor?: string;
+}
+
+export const UserAvatar = ({
+  src,
+  name,
+  fallback,
+  borderColor,
+}: UserAvatarProps) => {
+  return (
+    <Hint label={name || "Teammate"} side="bottom" sideOffset={18}>
+      <Avatar className="w-8 h-8 border-2" style={{ borderColor }}>
+        <AvatarImage src={src} />
+        <AvatarFallback className="text-xs font-semibold">
+          {fallback}
+        </AvatarFallback>
+      </Avatar>
+    </Hint>
+  );
 };
