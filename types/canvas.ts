8243c3d946f1acf9112e2b63501b607f16951235
myk <michaelmugendi1707@gmail.com>
2024-02-09 10:40:46 +0300

    Update: added more types

--- types/canvas.ts
+++ types/canvas.ts
@@ -63,31 +63,57 @@ export type NoteLayer = {
   value?: string;
 };
 
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type XYWH = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+export enum Side {
+  Top = 1,
+  Right = 2,
+  Bottom = 4,
+  Left = 8,
+}
+
 export type CanvasState =
   | {
       mode: CanvasMode.None;
     }
   | {
       mode: CanvasMode.SelectionNet;
+      origin: Point;
+      current?: Point;
     }
   | {
       mode: CanvasMode.Pressing;
+      origin: Point;
     }
   | {
       mode: CanvasMode.Translating;
+      current: Point;
     }
   | {
       mode: CanvasMode.Inserting;
+      layerType:
+        | LayerType.Reactangle
+        | LayerType.Ellipse
+        | LayerType.Note
+        | LayerType.Text;
     }
   | {
       mode: CanvasMode.Resizing;
+      initialBounds: XYWH;
+      corner: Side;
     }
   | {
       mode: CanvasMode.Pencil;
-      // x: number;
-      // y: number;
-      // width: number;
-      // height: number;
     };
 
 export enum CanvasMode {
