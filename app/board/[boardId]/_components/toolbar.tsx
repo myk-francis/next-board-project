adcbc9be4100745236148d24f05f4a3d3078c679
myk <michaelmugendi1707@gmail.com>
2024-02-09 09:58:41 +0300

    Update: added types file

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -11,7 +11,24 @@ import {
   Undo,
 } from "lucide-react";
 
-export const ToolBar = () => {
+type CanvasState = any;
+interface ToolBarProps {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+  onUndo: () => void;
+  onRedo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+}
+
+export const ToolBar = ({
+  canvasState,
+  setCanvasState,
+  onUndo,
+  onRedo,
+  canUndo,
+  canRedo,
+}: ToolBarProps) => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex flex-col gap-y-1 items-center shadow-md">
