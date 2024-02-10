4bc7018b336847a8935d1d2fe6e9cc29217b513b
myk <michaelmugendi1707@gmail.com>
2024-02-10 13:08:38 +0300

    Update: added type to liveblocks

--- app/board/[boardId]/_components/cursor.tsx
+++ app/board/[boardId]/_components/cursor.tsx
@@ -9,6 +9,15 @@ interface CursorProps {
 
 export const Cursor = memo(({ connectionId }: CursorProps) => {
   const info = useOther(connectionId, (user) => user?.info);
+  const cursor = useOther(connectionId, (user) => user.presence.cursor);
+
+  const name = info?.name || "Teammate";
+
+  if (!cursor) {
+    return null;
+  }
+
+  const { x, y } = cursor;
 
   return <div></div>;
 });
