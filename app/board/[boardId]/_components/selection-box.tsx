eddcb0654c07e33f2ee1bf02e3488039c38f2880
myk <michaelmugendi1707@gmail.com>
2024-02-11 12:28:35 +0300

    Update: implementation of SelectionBox

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useSelf, useStorage } from "@/liveblocks.config";
 import { LayerType, Side, XYWH } from "@/types/canvas";
 import React, { memo } from "react";
@@ -20,7 +21,27 @@ export const SelectionBox = memo(
       (root) =>
         soleLayerId && root.layers.get(soleLayerId)?.type === LayerType.Path
     );
-    return <div>SelectionBox</div>;
+
+    const bounds = useSelectionBounds();
+
+    if (!bounds) {
+      return null;
+    }
+
+    return (
+      <>
+        <rect
+          className="fill-transparent stroke-blue-500 stroke-1 pointer-events-none"
+          style={{
+            transform: `translate(${bounds.x}px, ${bounds.y}px)`,
+          }}
+          x={0}
+          y={0}
+          width={bounds.width}
+          height={bounds.height}
+        />
+      </>
+    );
   }
 );
 
