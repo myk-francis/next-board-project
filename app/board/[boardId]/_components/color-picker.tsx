85d68b4c2e74af2f2a18ee20b223dfb60d9775ae
myk <michaelmugendi1707@gmail.com>
2024-02-14 21:56:55 +0300

    Update: added color button

--- app/board/[boardId]/_components/color-picker.tsx
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -6,6 +6,15 @@ interface ColorPickerProps {
   onChange: (color: Color) => void;
 }
 
+interface ColorButtonProps {
+  color: Color;
+  onClick: () => void;
+}
+
+const ColorButton = ({ color, onClick }: ColorButtonProps) => {
+  return <div>Color Button!</div>;
+};
+
 export const ColorPicker = ({ onChange }: ColorPickerProps) => {
   return <div>Color Picker!</div>;
 };
