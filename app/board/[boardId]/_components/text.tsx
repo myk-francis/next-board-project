d787d946cf788e9324e2047772e06484bb290339
myk <michaelmugendi1707@gmail.com>
2024-02-15 12:01:40 +0300

    Update: added content to text component

--- app/board/[boardId]/_components/text.tsx
+++ app/board/[boardId]/_components/text.tsx
@@ -1,1 +1,39 @@
+import { TextLayer } from "@/types/canvas";
 import { Kalam } from "next/font/google";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+
+const font = Kalam({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+interface TextProps {
+  id: string;
+  layer: TextLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Text = ({
+  id,
+  layer,
+  onPointerDown,
+  selectionColor,
+}: TextProps) => {
+  const { x, y, width, height, fill, value } = layer;
+
+  return (
+    <foreignObject
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      style={{
+        outline: selectionColor ? `1px solid ${selectionColor}` : "none",
+      }}
+    >
+      <ContentEditable html={"Text"} onChange={() => {}} />
+    </foreignObject>
+  );
+};
