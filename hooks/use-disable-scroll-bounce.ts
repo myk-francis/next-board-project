837dc89140ab8e4ff03c61492da08047dde33bda
myk <michaelmugendi1707@gmail.com>
2024-02-15 14:40:12 +0300

    Update: implement useDisableScrollBounce

---
+++ hooks/use-disable-scroll-bounce.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const useDisableScrollBounce = () => {
+  React.useEffect(() => {
+    document.body.classList.add("overflow-hidden", "overscroll-none");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden", "overscroll-none");
+    };
+  }, []);
+};
