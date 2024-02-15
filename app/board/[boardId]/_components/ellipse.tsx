d6dc58315a287974dbc62c46588941f38ad0d096
myk <michaelmugendi1707@gmail.com>
2024-02-15 11:52:09 +0300

    Update: implemented ellipse

--- app/board/[boardId]/_components/ellipse.tsx
+++ app/board/[boardId]/_components/ellipse.tsx
@@ -1,9 +1,10 @@
+import { colorToCssColor } from "@/lib/utils";
 import { EllipseLayer } from "@/types/canvas";
 
 interface EllipseProps {
   id: string;
   layer: EllipseLayer;
-  onPointerDown: (e: React.PointerEvent, layerId: string) => void;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
   selectionColor?: string;
 }
 
@@ -13,5 +14,18 @@ export const Ellipse = ({
   onPointerDown,
   selectionColor,
 }: EllipseProps) => {
-  return <div>Mike</div>;
+  return (
+    <ellipse
+      className="drop-shadow-md"
+      style={{ transform: `translate(${layer.x}px, ${layer.y}px)` }}
+      onPointerDown={(e) => onPointerDown(e, id)}
+      cx={layer.width / 2}
+      cy={layer.height / 2}
+      rx={layer.width / 2}
+      ry={layer.height / 2}
+      fill={layer.fill ? colorToCssColor(layer.fill) : "#000"}
+      strokeWidth={1}
+      stroke={selectionColor || "transparent"}
+    />
+  );
 };
