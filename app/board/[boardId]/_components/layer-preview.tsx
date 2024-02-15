e2fa5b2f5403d17cef075a12707242055fa824b5
myk <michaelmugendi1707@gmail.com>
2024-02-15 13:35:10 +0300

    Update: added path component

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -21,6 +21,15 @@ export const LayerPreview = memo(
     }
 
     switch (layer.type) {
+      case LayerType.Path:
+        return (
+          <Path
+            key={id}
+            points={layer.points}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Note:
         return (
           <Note
